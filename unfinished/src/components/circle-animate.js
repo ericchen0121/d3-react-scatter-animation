95cc2fe674df419ac82683b7ff6d9340ffa7a40a
Eric Chen <ericchen0121@Erics-MacBook-Pro.local>
2017-04-04 17:47:03 -0700

    add animation circles

--- unfinished/src/components/circle-animate.js
+++ unfinished/src/components/circle-animate.js
@@ -8,11 +8,31 @@ class CircleAnimate extends Component {
     this.state = {
       cx: this.props.cx,
       cy: this.props.cy,
-      r: this.props.r
+      r: this.props.r,
+      className: 'enter'
     }
   }
 
+
+
   componentWillEnter(callback) {
+    const transition = d3.transition()
+                     .duration(750)
+                     .ease(d3.easeCubicInOut);
+
+    let node = d3.select(ReactDOM.findDOMNode(this));
+
+    node.transition(transition)
+      .attr('fill', 'red')
+      .attr('cx', this.props.cx)
+      .attr('cy', this.props.cy)
+      .on('end', () => {
+        this.setState({
+          cx: this.props.cx,
+          cy: this.props.cy
+        })
+        callback()
+      })
 
   }
 
@@ -21,13 +41,28 @@ class CircleAnimate extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    const transition = d3.transition()
+                     .duration(750)
+                    //  .ease(d3.easeCubicInOut);
+
     if (this.props.cx != nextProps.cx || this.props.cy != nextProps.cy) {
-      //start update transition
+      let node = d3.select(ReactDOM.findDOMNode(this));
+
+      node.on('end', () => {
+        this.setState({
+          cx: this.props.cx,
+          cy: this.props.cy
+        })
+      })
+      .transition(transition)
+        .attr('cx', this.props.cx)
+        .attr('cy', this.props.cy)
+
     }
   }
 
   render() {
-    return <circle {...this.props} />
+    return <circle {...this.state} />
   }
 }
 
