40cbcd25347a8cf62188caaded71a6e5010660f3
Eric Chen <ericchen0121@Erics-MacBook-Pro.local>
2017-04-04 18:50:57 -0700

    syncing state and props properly and defining d3 v3 transitions

--- unfinished/src/components/circle-animate.js
+++ unfinished/src/components/circle-animate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import d3 from 'd3';
+import './circle-animate.css'
 
 class CircleAnimate extends Component {
   constructor(props) {
@@ -13,23 +14,18 @@ class CircleAnimate extends Component {
     }
   }
 
-
-
   componentWillEnter(callback) {
-    const transition = d3.transition()
-                     .duration(750)
-                     .ease(d3.easeCubicInOut);
-
+    const { cx, cy } = this.props;
     let node = d3.select(ReactDOM.findDOMNode(this));
 
-    node.transition(transition)
+    node.transition().duration(750).ease('linear')
       .attr('fill', 'red')
-      .attr('cx', this.props.cx)
-      .attr('cy', this.props.cy)
-      .on('end', () => {
+      .attr('cx', cx)
+      .attr('cy', cy)
+      .each('end', () => {
         this.setState({
-          cx: this.props.cx,
-          cy: this.props.cy
+          cx: cx,
+          cy: cy
         })
         callback()
       })
@@ -41,23 +37,28 @@ class CircleAnimate extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    const transition = d3.transition()
-                     .duration(750)
-                    //  .ease(d3.easeCubicInOut);
-
-    if (this.props.cx != nextProps.cx || this.props.cy != nextProps.cy) {
+    const { index, cx, cy, r } = nextProps;
+    if (this.props.cx != cx || this.props.cy != cy) {
       let node = d3.select(ReactDOM.findDOMNode(this));
 
-      node.on('end', () => {
-        this.setState({
-          cx: this.props.cx,
-          cy: this.props.cy
-        })
-      })
-      .transition(transition)
-        .attr('cx', this.props.cx)
-        .attr('cy', this.props.cy)
+      this.setState({className: 'update'});
 
+      node.transition().duration(750)
+        .delay(index * 10)
+        .attr('cx', cx)
+        .attr('cy', cy)
+        .attr('fill', 'red')
+        .attr('r', r * 1.5)
+      .transition().duration(350)
+        .attr('fill', 'black')
+        .attr('r', r)
+      .each('end', () => {
+          this.setState({
+            cx: cx,
+            cy: cy,
+            className: 'enter'
+          })
+        })
     }
   }
 
